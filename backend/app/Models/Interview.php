1585d40a5b7994e2a92b718e72a533116b35e2a1
nabilrobbani84 <nabilrobbani6@gmail.com>
2025-05-09 21:58:50 +0700

    Update Interview.php

--- backend/app/Models/Interview.php
+++ backend/app/Models/Interview.php
@@ -2,9 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Interview extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'application_id',
+        'interview_date',
+        'interview_time',
+        'interview_location',
+        'status'
+    ];
 }
