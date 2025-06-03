05729dedbeb59908402414701c7e8a998a42870e
nabilrobbani84 <nabilrobbani6@gmail.com>
2025-06-04 01:15:01 +0700

    Update CandidateController.php

--- backend/app/Http/Controllers/CandidateController.php
+++ backend/app/Http/Controllers/CandidateController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class CandidateController extends Controller
+class CandidateController 
 {
     /**
      * Display a listing of the resource.
