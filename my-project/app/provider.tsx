c0a08046f61f878f558118369e8204b6704823aa
nabilrobbani84 <nabilrobbani6@gmail.com>
2025-07-05 23:07:55 +0700

    Update provider.tsx

--- my-project/app/provider.tsx
+++ my-project/app/provider.tsx
@@ -1,12 +1,20 @@
-"use client";
+"use client"; // Tanda ini sangat penting!
 
-import { Provider } from "react-redux";
-import { store } from "../redux/store";
+import React from "react";
+import { Provider as ReduxProvider } from "react-redux";
+import { store } from "@/redux/store"; // Pastikan path ini benar
+import { AuthProvider } from "@/contexts/AuthContext"; // 1. Impor AuthProvider
 
-// Menentukan tipe untuk children menggunakan React.ReactNode
-const ProviderRedux = ({ children }: { children: React.ReactNode }) => {
-  return <Provider store={store}>{children}</Provider>;
+// Mengganti nama `Provider` dari Redux agar tidak bentrok
+const Providers = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <ReduxProvider store={store}>
+            {/* 2. Bungkus children dengan AuthProvider di sini */}
+            <AuthProvider>
+                {children}
+            </AuthProvider>
+        </ReduxProvider>
+    );
 };
 
-export default ProviderRedux;
- 
+export default Providers;
