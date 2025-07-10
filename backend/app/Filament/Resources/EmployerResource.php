6674c965a63618b80568c5b6415977b1096b2836
nabilrobbani84 <nabilrobbani6@gmail.com>
2025-07-10 23:52:12 +0700

    Create EmployerResource.php

---
+++ backend/app/Filament/Resources/EmployerResource.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\EmployerResource\Pages;
+use App\Filament\Resources\EmployerResource\RelationManagers;
+use App\Models\Employer;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class EmployerResource extends Resource
+{
+    protected static ?string $model = Employer::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ManageEmployers::route('/'),
+        ];
+    }
+}
