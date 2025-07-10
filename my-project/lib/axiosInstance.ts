4a8bf031f126ca572398803afbca1e844daaec03
nabilrobbani84 <nabilrobbani6@gmail.com>
2025-07-11 00:05:32 +0700

    Update axiosInstance.ts

--- my-project/lib/axiosInstance.ts
+++ my-project/lib/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://api.example.com/', // Ganti dengan URL API Anda
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL, // Ganti dengan URL API Anda
   timeout: 10000, // Waktu tunggu request (10 detik)
   headers: {
     'Content-Type': 'application/json',
