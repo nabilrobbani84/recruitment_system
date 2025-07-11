875c4dcdf1933385e333c4b4df6819ff867b7fdb
nabilrobbani84 <nabilrobbani6@gmail.com>
2025-07-11 22:42:10 +0700

    Create ManageCandidates.php

---
+++ backend/app/Filament/Resources/CandidateResource/Pages/ManageCandidates.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Resources\CandidateResource\Pages;
+
+use App\Filament\Resources\CandidateResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ManageRecords;
+
+class ManageCandidates extends ManageRecords
+{
+    protected static string $resource = CandidateResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+        ];
+    }
+}
